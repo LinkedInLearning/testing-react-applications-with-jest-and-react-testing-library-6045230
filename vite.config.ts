4ec8dc451a8915b992c235a412459a76f780566c
Chiomadans <oluchukwu.alooba.com>
2025-05-30 20:35:10 +0200

    02_03

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig, coverageConfigDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -8,5 +8,13 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+    coverage: {
+      provider: 'v8', // Use V8's built-in coverage tool
+      reporter: ['text', 'json', 'html'], // Generate reports in multiple formats
+      exclude: [
+        ...coverageConfigDefaults.exclude, // Use Vitest's default excluded files
+        'src/main.tsx', // Exclude entry point files
+      ],
+    },
   },
 })
