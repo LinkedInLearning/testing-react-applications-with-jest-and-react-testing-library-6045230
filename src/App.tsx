743364b35ef27087fdfe1ceee0198672ca633f91
Chiomadans <oluchukwu.alooba.com>
2025-05-31 08:43:19 +0200

    03_01

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
-import './App.css'
-import Button from './components/Button'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import { AuthProvider } from './contexts/AuthContext';
+import { Navbar } from './components/Navbar';
+import { Home } from './pages/Home';
+import { Post } from './pages/Post';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { CreatePost } from './pages/CreatePost';
+
+const AppContainer = styled.div`
+  min-height: 100vh;
+  background-color: #f9fafb;
+`;
+
+const MainContent = styled.main`
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem 1rem;
+`;
 
 function App() {
   return (
-    <div>
-      <h1>
-        Welcome To Blog App
-      </h1>
-      <Button label='Create a post' onClick={() => {}}/>
-    </div>
-  )
+    <AuthProvider>
+      <Router>
+        <AppContainer>
+          <Navbar />
+          <MainContent>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/post/:id" element={<Post />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/create" element={<CreatePost />} />
+            </Routes>
+          </MainContent>
+        </AppContainer>
+      </Router>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
